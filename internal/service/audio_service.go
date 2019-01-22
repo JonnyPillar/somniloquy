d3880ed5a174415683f68632d0449e0b97219811
Jonny Pillar <info@jonnypillar.co.uk>
2019-01-22 11:29:20 +0000

    Added naive Google Cloud Speech integration

--- internal/service/audio_service.go
+++ internal/service/audio_service.go
@@ -1,12 +1,16 @@
 package service
 
 import (
-	"io"
+	"context"
+	"fmt"
+	"io/ioutil"
 	"log"
 
+	"cloud.google.com/go/speech/apiv1"
 	"github.com/jonnypillar/somniloquy/configs"
 	"github.com/jonnypillar/somniloquy/internal/api"
 	"github.com/pkg/errors"
+	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
 	"google.golang.org/grpc"
 )
 
@@ -26,25 +30,69 @@ func NewAudioService(config *config.ServiceConfig, grpcServer *grpc.Server) {
 
 // Upload ...
 func (s *AudioService) Upload(stream api.AudioService_UploadServer) error {
-	r := Recording{}
-
-	for {
-		c, err := stream.Recv()
-
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-
-			return errors.Wrap(err, "failed unexpectadely while reading chunks from stream")
-		}
-
-		r.Append(c.Content)
+	ctx := context.Background()
+	client, err := speech.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
 	}
 
-	err := r.Save()
+	r := Recording{
+		File: "test_one",
+	}
+
+	// r := NewRecording()
+
+	// for {
+	// 	c, err := stream.Recv()
+
+	// 	if err != nil {
+	// 		if err == io.EOF {
+	// 			break
+	// 		}
+
+	// 		return errors.Wrap(err, "failed unexpectadely while reading chunks from stream")
+	// 	}
+
+	// 	r.Append(c.Content)
+	// }
+
+	// err = r.Save()
+	// if err != nil {
+	// 	return errors.Wrapf(err, "failed to save recording")
+	// }
+
+	// if err := exec.Command("ffmpeg", "-i", "./assets/recordings/test_one.aiff", "-c:a", "flac", "./assets/recordings/test_one.flac").Run(); err != nil {
+	// 	log.Fatalf("FFMPEG Error: %v", err)
+	// }
+
+	// Reads the audio file into memory.
+	data, err := ioutil.ReadFile("./assets/recordings/" + r.File + ".flac")
 	if err != nil {
-		return errors.Wrapf(err, "failed to save recording")
+		log.Fatalf("Failed to read file: %v", err)
+	}
+
+	// Detects speech in the audio file.
+	resp, err := client.Recognize(ctx, &speechpb.RecognizeRequest{
+		Config: &speechpb.RecognitionConfig{
+			Encoding:        speechpb.RecognitionConfig_FLAC,
+			SampleRateHertz: 44100,
+			LanguageCode:    "en-GB",
+		},
+		Audio: &speechpb.RecognitionAudio{
+			AudioSource: &speechpb.RecognitionAudio_Content{Content: data},
+		},
+	})
+	if err != nil {
+		log.Fatalf("failed to recognize: %v", err)
+	}
+
+	fmt.Println("Response:", resp)
+
+	// Prints the results.
+	for _, result := range resp.Results {
+		for _, alt := range result.Alternatives {
+			fmt.Printf("\"%v\" (confidence=%3f)\n", alt.Transcript, alt.Confidence)
+		}
 	}
 
 	status := api.UploadStatus{
